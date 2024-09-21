5acfcd9f655222a59c61814398551699ff551481
Minki Kim <mingi3314@gmail.com>
2024-09-22 01:46:29 +0900

    💄 tailwind primevue 적용

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
+import PrimeVue from "primevue/config";
 import App from "./App.vue";
 import router from "./router";
 import { createPinia } from "pinia";
@@ -8,5 +9,7 @@ const pinia = createPinia();
 
 app.use(router); // Use the router
 app.use(pinia); // Register Pinia with the Vue app
-
+app.use(PrimeVue, {
+  unstyled: true,
+});
 app.mount("#app");
