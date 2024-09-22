7b74e1d321582c817aca29eff4e02ad6a370273a
Minki Kim <68267535+mingi3314@users.noreply.github.com>
2024-09-22 18:53:25 +0900

    💄 migrate HomePage.vue (#40)

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import PrimeVue from "primevue/config";
 import App from "./App.vue";
 import router from "./router";
 import { createPinia } from "pinia";
+import Lara from "./presets/lara";
 
 const app = createApp(App);
 const pinia = createPinia();
@@ -11,5 +12,6 @@ app.use(router); // Use the router
 app.use(pinia); // Register Pinia with the Vue app
 app.use(PrimeVue, {
   unstyled: true,
+  pt: Lara,
 });
 app.mount("#app");
---
+++ src/presets/lara/index.d.ts
@@ -0,0 +1,12 @@
+declare const Lara: {
+  global: any;
+  directives: {
+    badge: any;
+    ripple: any;
+    tooltip: any;
+  };
+  // Add other component types here
+  [key: string]: any;
+};
+
+export default Lara;
