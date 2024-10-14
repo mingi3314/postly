3d2a3a3df8769d0650fa8fdb6dc9a491a861debd
Minki Kim <mingi3314@gmail.com>
2024-10-15 00:20:21 +0900

    🚸 show toast for signin/up failure

--- src/pages/SignInPage.vue
+++ src/pages/SignInPage.vue
@@ -1,24 +1,30 @@
 <template>
-  <AuthForm
-    title="로그인"
-    submitLabel="로그인"
-    footerLink="/signup"
-    footerText="계정이 없으신가요? 회원가입"
-    @submit="handleSignIn"
-  />
+  <div>
+    <AuthForm
+      title="로그인"
+      submitLabel="로그인"
+      footerLink="/signup"
+      footerText="계정이 없으신가요? 회원가입"
+      @submit="handleSignIn"
+    />
+    <Toast position="top-right" />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import { useRouter } from "vue-router";
 import { useUserStore } from "../stores/userStore";
+import { useToast } from "../composables/useToast";
 import AuthForm from "@/components/AuthForm.vue";
+import Toast from "primevue/toast";
 
 export default defineComponent({
-  components: { AuthForm },
+  components: { AuthForm, Toast },
   setup() {
     const router = useRouter();
     const userStore = useUserStore();
+    const { showError } = useToast();
 
     const handleSignIn = async ({
       email,
@@ -32,7 +38,7 @@ export default defineComponent({
         router.push("/");
       } catch (error) {
         console.error("Sign in failed:", error);
-        // TODO: Show error message to user
+        showError("로그인에 실패했습니다. 이메일과 비밀번호를 확인해주세요.");
       }
     };
 
--- src/pages/SignUpPage.vue
+++ src/pages/SignUpPage.vue
@@ -1,24 +1,30 @@
 <template>
-  <AuthForm
-    title="회원가입"
-    submitLabel="회원가입"
-    footerLink="/signin"
-    footerText="이미 계정이 있으신가요? 로그인"
-    @submit="handleSignUp"
-  />
+  <div>
+    <AuthForm
+      title="회원가입"
+      submitLabel="회원가입"
+      footerLink="/signin"
+      footerText="이미 계정이 있으신가요? 로그인"
+      @submit="handleSignUp"
+    />
+    <Toast position="top-right" />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import { useRouter } from "vue-router";
 import { useUserStore } from "../stores/userStore";
+import { useToast } from "../composables/useToast";
 import AuthForm from "@/components/AuthForm.vue";
+import Toast from "primevue/toast";
 
 export default defineComponent({
-  components: { AuthForm },
+  components: { AuthForm, Toast },
   setup() {
     const router = useRouter();
     const userStore = useUserStore();
+    const { showError } = useToast();
 
     const handleSignUp = async ({
       email,
@@ -32,7 +38,9 @@ export default defineComponent({
         router.push("/");
       } catch (error) {
         console.error("Sign up failed:", error);
-        // TODO: Show error message to user
+        showError(
+          "회원가입에 실패했습니다. 문제가 계속되면 관리자에게 문의해주세요."
+        );
       }
     };
 
